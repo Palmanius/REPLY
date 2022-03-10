022c0cbb26954f492fe523994bd05d9a146dca5e
Palmanius <hrdy@gsedu.sk>
2022-03-10 15:48:43 +0100

    Yes

--- Task3/main.py
+++ Task3/main.py
@@ -1,5 +1,11 @@
-with open("Task1\\Input.txt","r") as Input:
+with open("Task3\\Input.txt","r") as Input:
     with open("Task1\\Output.txt","w") as Output:
         T = int(Input.readline().strip())
 
-print(T)
+        for cases in range(1,T+1):
+            
+
+
+
+            solution = 0
+            Output.writelines("Case #"+str(T)+": "+str(solution)+"\n")
